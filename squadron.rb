f2d5a3bc286a5d80843a7a5cb51223877d5aa459
goreleaserbot <goreleaser@carlosbecker.com>
2021-02-16 11:54:11 +0100

    Brew formula update for squadron version v0.8.7

--- squadron.rb
+++ squadron.rb
@@ -5,16 +5,16 @@
 class Squadron < Formula
   desc "CLI utility manage infrastructure as code with helm"
   homepage "https://github.com/foomo/squadron"
-  version "0.8.6"
+  version "0.8.7"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/foomo/squadron/releases/download/v0.8.6/squadron_0.8.6_darwin_amd64.tar.gz"
-    sha256 "8fe28e9a0e2d8711e4aa7c40ad3c701840230057f7a7f5dce1c3ecb3e7e49701"
+    url "https://github.com/foomo/squadron/releases/download/v0.8.7/squadron_0.8.7_darwin_amd64.tar.gz"
+    sha256 "1290c3f9a848801eebc00f86baf957ad1c8a802f68236a949de3ba5df39d3f60"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/foomo/squadron/releases/download/v0.8.6/squadron_0.8.6_linux_amd64.tar.gz"
-    sha256 "2c52b6c194dfc1b77ef90c4bdc7fb1f9850997a99585ba0f8e2307e8fc19be11"
+    url "https://github.com/foomo/squadron/releases/download/v0.8.7/squadron_0.8.7_linux_amd64.tar.gz"
+    sha256 "21e18a60932a35c64f71277f62aa35390587912299b24330bf005cc7883b7515"
   end
 
   def install
